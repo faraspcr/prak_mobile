bc4053f9364666704b0fb3948e46b05f59649cf4
Pertemuan2 <faras24si@mahasiswa.pcr.ac.id>
2026-04-08 10:07:36 +0700

    sc commit 4

--- app/src/main/java/com/example/labubu_apps/MainActivity.kt
+++ app/src/main/java/com/example/labubu_apps/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.labubu_apps
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.labubu_apps.databinding.ActivityMainBinding
+import com.example.labubu_apps.databinding.ActivityThirdBinding
+import com.example.labubu_apps.pertemuan_3.ThirdResultActivity
+import com.example.labubu_apps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnToFourth.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+            startActivity(intent)
+            finish()
         }
     }
 }
